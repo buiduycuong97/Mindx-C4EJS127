df65e4e6bc75179eaa0c5e8301489d7acce40475
buiduycuong97 <hoanganhbk1235@gmail.com>
2022-04-10 18:39:30 +0700

    update trainobj

--- Trainning/trainObj.js
+++ Trainning/trainObj.js
@@ -1,16 +1,21 @@
-< p > Multiply every element in the array with 10: < /p>
+const Drink = function(name, price, material, vote, preserve) {
+    this.name = name;
+    this.price = price;
+    this.material = material;
+    this.vote = vote;
+    this.preserve = preserve;
+}
 
-<
-p id = "demo" > < /p>
+let sodaMix = new Drink(
+    'soda', '40k', ['soda', 'sugar', 'syrup', 'lemon'],
+    '***', {
+        notOpen: { temperature: 'normal', date: '2year' },
+        opened: { temperature: 'cold', date: '6month' }
+    });
 
-<
-script >
-    const numbers = [65, 44, 12, 4];
-const newArr = numbers.map(myFunction);
+for (let key in sodaMix.preserve) {
+    for (let i in sodaMix.preserve[key]) {
+        console.log(`${key} - ${i} : ${sodaMix.preserve[key][i]}  `);
+    }
 
-document.getElementById("demo").innerHTML = newArr;
-
-function myFunction(num, index, arr) {
-    return num * 10;
-} <
-/script>
+}
